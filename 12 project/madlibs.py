befaa59268e234c0f0ac49fd51f80b5bcf89306a
Aashutosh Bharti <125398000+ashh106@users.noreply.github.com>
2025-01-17 18:33:55 +0530

    Day 1 : madlibs
    # string concatination
    
    # youtuber = "ashh"
    # print("sunscribe to " + youtuber)
    # print("subscribe to {}".format(youtuber))
    # print(f"subscribe to {youtuber}")

---
+++ 12 project/madlibs.py
@@ -0,0 +1,14 @@
+# string concatination
+
+# youtuber = "ashh"
+# print("sunscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous_person = input("famous person: ")
+madlib = f"Computer programming is so {adj}! It makes me so exited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you're {famous_person}!" 
+print(madlib)
